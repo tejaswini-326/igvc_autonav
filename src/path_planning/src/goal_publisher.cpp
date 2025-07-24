5a324057d50a657153c18e3767811e7e9b0d77ff
sanika_n <nair.sanika.pradeep@gmail.com>
2025-07-24 11:56:31 +0530

    idkopsd

--- src/path_planning/src/goal_publisher.cpp
+++ src/path_planning/src/goal_publisher.cpp
@@ -84,10 +84,6 @@ public:
 #ifdef RVIZ_DISTANCE_DEBUG
         distance_viz_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("/a_goalpub_debug_distances", 10);
 #endif
-        override_ = "none";
-        target_lane_ = "right";
-
-        lane_history_memory_buffer_size_ = 10;
         pothole_detected_ = false;
         pothole_pos_.x = pothole_pos_.y = pothole_pos_.z = 0.0;
         RCLCPP_INFO(this->get_logger(), "GoalPublisher node initialized");
